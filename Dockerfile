4c4bbcbbda24781eae29295f169d0d668c96a050
sasa <sasasoni1119@gmail.com>
2022-10-16 00:45:01 +0900

    add requirements

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM public.ecr.aws/lambda/python:3.9
 
 COPY app.py ${LAMBDA_TASK_ROOT}
+COPY .env ${LAMBDA_TASK_ROOT}
+COPY requirements.txt ${LAMBDA_TASK_ROOT}
+
+RUN pip3 install -r requirements.txt
 
 CMD ["app.handler"]
--- app.py
+++ app.py
@@ -1,4 +1,11 @@
+from os.path import join, dirname
+from dotenv import load_dotenv
+import os
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path=dotenv_path)
+
 def handler(event, context):
-  print(f"from lambda:\n{event}")
+  print(f"CUSTOM_ENV: {os.getenv('CUSTOM_ENV')}")
 
   return event
