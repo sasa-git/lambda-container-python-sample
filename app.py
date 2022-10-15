32db9be8f7b0dca2786ab14acdb421bdc8ef1752
sasa <sasasoni1119@gmail.com>
2022-10-16 01:26:06 +0900

    add file read write sample

--- app.py
+++ app.py
@@ -8,4 +8,14 @@ load_dotenv(dotenv_path=dotenv_path)
 def handler(event, context):
   print(f"CUSTOM_ENV: {os.getenv('CUSTOM_ENV')}")
 
+  file_path = f'/tmp/sample.txt'
+
+  with open(file_path, 'w') as f:
+    f.write("hello, SRE Sunday!")
+
+  with open(file_path) as f:
+    s = f.read()
+
+  print(s)
+
   return event
