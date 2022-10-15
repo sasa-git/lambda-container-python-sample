28842dd7414b5fe141d26a19178d5e190e62d0a9
sasa <sasasoni1119@gmail.com>
2022-10-16 02:38:29 +0900

    add logger

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 import os
 import json
+from aws_lambda_powertools import Logger
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path=dotenv_path)
 
+logger = Logger(level='INFO', service='sample-lambda')
+
 def handler(event, context):
-  print(os.environ)
+  logger.info(event)
 
   return {
     'statusCode': 200,
