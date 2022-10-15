35cba4647444a5f244eec6a97047d992f93db4c7
sasa <sasasoni1119@gmail.com>
2022-10-16 02:34:35 +0900

    add for lambda example

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from os.path import join, dirname
 from dotenv import load_dotenv
 import os
+import json
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path=dotenv_path)
 
 def handler(event, context):
-  print(f"CUSTOM_ENV: {os.getenv('CUSTOM_ENV')}")
+  print(os.environ)
 
-  return event
+  return {
+    'statusCode': 200,
+    'headers': {"Content-Type": "application/json"},
+    'body': json.dumps(event),
+    'isBase64Encoded': False
+  }
