508f3b1598f7aba1c78d203fcba82774fb10133d
sasa <sasasoni1119@gmail.com>
2022-10-16 02:17:21 +0900

    add access-to-eks

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import subprocess
 from os.path import join, dirname
 from dotenv import load_dotenv
 import os
@@ -7,5 +8,10 @@ load_dotenv(dotenv_path=dotenv_path)
 
 def handler(event, context):
   print(f"CUSTOM_ENV: {os.getenv('CUSTOM_ENV')}")
+
+# eks 上のクラスタにアクセスする
+  eks_cluster_name = 'sample-cluster'
+  subprocess.run(['aws', 'eks', '--region', 'ap-northeast-1', 'update-kubeconfig', '--name', eks_cluster_name, '--kubeconfig', '/tmp/kubeconfig'], text=True)
+  subprocess.run(['kubectl', 'get', 'node', '--kubeconfig', '/tmp/kubeconfig'], text=True)
 
   return event
